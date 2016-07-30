81df7e4b33c56f6293080266dc9874b89ecca08d
weeneen02 <weeneen02@gmail.com>
2016-07-31 00:31:08 +0900

    start coding

---
+++ beetle.ino
@@ -0,0 +1,20 @@
+
+/*
+ 
+   This program is for beetle to comunicate with 
+   raspberry or other Thing using bluetooth.
+    
+   in this program, it should send data to raspberry
+   and make a public key for encrpyt.
+ */
+
+void setup() {
+
+}
+
+
+void loop() {
+
+}
+
+
