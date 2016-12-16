6543f158a99967c69b317c17685e3c7fdda248e9
hwan <weeneen02@gmail.com>
2016-12-17 00:18:06 +0900

    add origin

--- beetle/origin/ilock.cpp
+++ beetle/origin/ilock.cpp
@@ -1,25 +1,27 @@
 /*
-   finish
-   should add..
-   1. button interrupt.
-   2. timer value changes.
-   3.
+   Project Name : Ilock
+Authors      : 임재환, 문지인
+Functions    : Operating the servo motor when specific conditionals meet.
+1. Pairing with Bluno beetles
+2. Analyze the Pubilc key
+3. Open the Door or Close it.
+
+Note         :
+DeadLine     : 2016_12_18   
  */
 
 
 #include<Servo.h>
-#define PERIPHERAL
-#define DEVINUM 1
+#define CENTRAL
+#define DEVINUM 2
 
-#ifdef CENTRAL
+/*-------------------CENTRAL BLUNO-----------------------*/
+#ifdef CENTRAL      
 #define SETTIME 10000000
 
 /* variables */
-int buttonPin = 5;
-int buttonState = 0;
-
 String readbuf = "";
-String mac[2] = {"0xC4BE84E3A786", "0xC4BE84DE3D3F"};
+//String mac[DEVINUM] = {"0xC4BE84E3A786"};
 typedef enum {AT_MODE = 0, NORM_MODE = 1} modeState;
 modeState at_state = NORM_MODE;
 int bindOrWait = 0;
@@ -30,18 +32,16 @@ Servo myservo;
 int pos = 0;
 
 void openDoor(void) {
-    mode = 0;
-    for ( pos = 0; pos <= 130; pos += 1) {
+    for ( pos = 0; pos <= 180; pos += 1) {
         myservo.write(pos);
-        delay(2);
+        delay(5);
     }
 }
 
 void closeDoor(void) {
-    mode = 1;
-    for ( pos = 130; pos >= 0; pos -= 1) {
+    for ( pos = 180; pos >= 0; pos -= 1) {
         myservo.write(pos);
-        delay(2);
+        delay(5);
     }
 }
 
@@ -65,45 +65,18 @@ void loop(void) {
 
             digitalWrite(2, LOW);
             digitalWrite(4, HIGH);
-            /*
-               Serial.print("+");
-               Serial.print("+");
-               Serial.print("+");
-               delay(1000);
-
-               if ( mac_index >= DEVINUM ) {
-               mac_index %= DEVINUM;
-               }
-
-               Serial.println("AT+BIND=" + mac[mac_index++]);
-               delay(1000);
-
-               Serial.println("AT+RESTART");
-               delay(5000);
-             */
             bindOrWait = 1;
-
             t1 = micros();
             break;
 
         case 1:
-            readbuf = "";
             /* send data to peri*/
-            Serial.flush();
             digitalWrite(2, HIGH);
             digitalWrite(4, LOW);
             Serial.println(mode);  /*0 is open 1 is closed*/
             Serial.flush();
 
             while ( micros() - t1 <= SETTIME ) {
-                buttonState = digitalRead(buttonPin);
-                if (buttonState == HIGH) {
-                    if ( mode == 0 ) {
-                        closeDoor();
-                    } else if ( mode == 1 ) {
-                        openDoor();
-                    }
-                }
                 while ( Serial.available() > 0 ) {
                     chr = Serial.read();
                     readbuf += chr;
@@ -111,34 +84,20 @@ void loop(void) {
                     if ( readbuf.indexOf("open") >= 0 ) {
                         if ( mode == 0 ) {
                             /* already open. */
-                            closeDoor();
+                            openDoor();
+                            mode = 1;
 
                         } else if ( mode == 1 ) {
-                            openDoor();
-
+                            closeDoor();
+                            mode = 0;
                         }
 
                         readbuf = "";
-                        if ( micros() - t1 >= SETTIME ) {
+                        if ( micros() - t1 <= SETTIME ){
                             bindOrWait = 0;
                             break;
                         }
                     } 
-                    /*else if ( readbuf.indexOf("close") >= 0 ) {
-                      if ( mode == 1 ) {
-
-                      } else if ( mode == 0 ) {
-                      closeDoor();
-
-                      }
-                      readbuf = "";
-
-                      if ( micros() - t1 >= SETTIME ) {
-                      bindOrWait = 0;
-                      break;
-                      }
-                      }
-                     */
                 }
                 if ( bindOrWait == 0 ) {
                     break;
@@ -150,37 +109,41 @@ void loop(void) {
 }
 #endif
 
+/*--------------------------------END--------------------------------*/
 
+/*------------------------PERIPHERIAL BLUNO--------------------------*/
 #ifdef  PERIPHERAL
 int openPin = 5;
 int openButState = 0;
-
+int closePin = 2;
+int closeButState = 0;
 int mode = 0;
 String readBuf = "";
 char chr = 'v';
 
 void setup(void) {
     pinMode(openPin, INPUT);
-
+    pinMode(closePin, INPUT);
     Serial.begin(115200);
 }
 
 void loop(void) {
     openButState = digitalRead(openPin);
+    closeButState = digitalRead(closePin);
+    chr = 'v'; 
 
-    chr = 'v';
-
-    while (Serial.available() > 0 ) {
+    while(Serial.available() > 0 ){
         chr = Serial.read();
     }
 
-    if (chr == 'v') {
+    if (chr == 'v'){
         /*nothing to come*/
-    } else if ( chr == '1' ) {
+    } else if ( chr == '1' ){
         /*The door is now close state*/
-    } else if ( chr == '0' ) {
+        mode = 1;
+    } else if ( chr == '0' ){
         /*The door is now open state*/
-
+        mode = 0;
     }
 
     if ( openButState == HIGH ) {
@@ -188,11 +151,7 @@ void loop(void) {
         Serial.write("open");
         delay(1000);
     } 
-    /*else if ( closeButState == HIGH ) {
-      Serial.write("close");
-      delay(1000);
-      }
-     */
 }
 
 #endif
+/*------------------------------END--------------------------------*/
